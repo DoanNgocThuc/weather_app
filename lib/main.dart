1fecbbcf6330d11a67f23d552e344bd625fc59c5
DoanNgocThuc <dnthuc1212@gmail.com>
2025-08-14 17:21:06 +0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/providers/weather_provider.dart';
 import 'screens/home_screen.dart';
+import '../firebase_options.dart';
+
 
 Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: ".env"); // <-- Load BEFORE runApp
   runApp(
     MultiProvider(
